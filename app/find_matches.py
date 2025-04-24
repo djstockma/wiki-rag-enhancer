2bd36757377c92566edac71b04f7099183655941
Jens <jens.w.stockmann@gmail.com>
2025-04-24 11:05:36 +0200

    Article lookup

--- app/find_matches.py
+++ app/find_matches.py
@@ -1,4 +1,4 @@
-from utils.db import find_best_match
+from utils.db import find_best_match, get_relevant_article_counts
 from utils.embedding import embed_text
 from utils.db import get_connection
 
@@ -7,3 +7,9 @@ def find_matches(text, n=1):
     embedded = embed_text(text)
     result = find_best_match(conn, embedded, n)
     return result
+
+def find_relevant_articles(text, n=1000):
+    conn = get_connection()
+    embedded = embed_text(text)
+    counts = get_relevant_article_counts(conn, embedded, n=n)
+    return counts
--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import sys
 import requests
 from fetch_wikipedia import fetch_pages
 from embed import embed_articles
-from find_matches import find_matches
+from find_matches import find_matches, find_relevant_articles
 
 time.sleep(4) # Gives mariadb time to start
 
@@ -16,14 +16,20 @@ def main():
     #titles = ["Helsingfors", "Åbo", "Nykarleby", "Kokkola", "Mattlidens gymnasium", "Aalto university", "München", "Pope Franciskus", "Hotwheels"]
     #fetch_pages(titles=titles)              # saves raw_wiki_content.json
     
-    #embed_articles()       # Go through the json, embed chunks and insert into db
+    embed_articles()       # Go through the json, embed chunks and insert into db
     
-    input_text = "Some random yappayappa about the pope and the vatican city. The catholic church. May Pope franciskus rest in peace."
+    input_text = "Here we can yap about cars and tracks, which should point tot hotwheels"
     result = find_matches(input_text, 4) # Number of matches is optional, default=1
     print(f"Here are the top {len(result)} result(s):")
     for i, chunk in enumerate(result):
         print(f"Chunk {i + 1} (from article '{chunk[3]}'): {chunk[1]}")
     # print(f"Article name: {result[3]} \n\n Content: {result[1]}")
+
+
+    # Here we look at the n(=10) closest chunks, and see what article they belong to
+    print("articles:")
+    for key, value in find_relevant_articles(input_text, n=10).items():
+        print(f"name: {key}, occurances: {value}")
 
 if __name__ == "__main__":
     main()
