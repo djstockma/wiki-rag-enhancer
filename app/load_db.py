8ee651805bbf13a2587e903b7dccccd0f02f8616
Jens <jens.w.stockmann@gmail.com>
2025-05-03 10:46:25 +0200

    logging

--- app/load_db.py
+++ app/load_db.py
@@ -5,11 +5,11 @@ import torch
 from utils.db import get_connection, insert_embedding, delete_embeddings
 from utils.text_utils import chunk_text
 import pandas as pd
-import logging
+from utils.logging_config import get_logger
 
 torch.classes.__path__ = [] # Fixes error: Tried to instantiate class '__path__._path', but it does not exist! Ensure that it is registered via torch::class_
 
-logger = logging.getLogger(__name__)
+logger = get_logger()
 
 def get_titles(file_path: str) -> list:
     print(f"Reading file and 'Wikidata' column: {file_path}")
@@ -19,10 +19,7 @@ def get_titles(file_path: str) -> list:
     return wikidata_list
 
 
-def embed_articles(conn, pages: dict, language="en") -> int:
-    logger.info(" Loading embedding model...")
-    model = SentenceTransformer("all-MiniLM-L6-v2")
-
+def embed_articles(conn, pages: dict, model, language="en") -> int:
     for title, content in pages.items():
         chunks = chunk_text(content)
         embeddings = model.encode(chunks, convert_to_numpy=True)
@@ -39,7 +36,7 @@ def embed_articles(conn, pages: dict, language="en") -> int:
     return(len(pages.items()))
 
 
-def fetch_pages_batch(titles, lang="en", batch_size=50, sleep_time=0.5) -> int:
+def fetch_pages_batch(titles, lang="en", batch_size=100, sleep_time=0.5) -> int:
     wiki_wiki = wikipediaapi.Wikipedia(user_agent='wiki_rag_enhancer (jens.w.stockmann@gmail.com)', language=lang)
     total_pages = len(titles)
 
@@ -49,6 +46,8 @@ def fetch_pages_batch(titles, lang="en", batch_size=50, sleep_time=0.5) -> int:
     logger.info("Truncating old embedding table...")
     delete_embeddings(conn)
     total = 0
+    logger.info("Loading embedding model...")
+    model = SentenceTransformer("all-MiniLM-L6-v2")
     for start_idx in range(0, total_pages, batch_size):
         end_idx = min(start_idx + batch_size, total_pages)
         batch_titles = titles[start_idx:end_idx]
@@ -63,7 +62,7 @@ def fetch_pages_batch(titles, lang="en", batch_size=50, sleep_time=0.5) -> int:
                 logger.info(f"Page not found: {title}")
 
         # Here you would insert `pages` into your database
-        embed_articles(conn=conn, pages=pages, language=lang)
+        embed_articles(conn=conn, model=model, pages=pages, language=lang)
 
         logger.info(f"{total} pages total in database.")
 
